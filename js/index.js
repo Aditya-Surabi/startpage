587556b6cd1228ccd607e7ad51f09a0637912e97
Aditya-Surabi <adityasurabi@gmail.com>
2017-05-13 18:11:44 -0400

    fixed format error for minutes in time display

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ function getTime(){
 h = time.getHours();
 if (h > 12){h = h-12};
 m = time.getMinutes();
+if (m < 10){m = '0'+m};
 document.getElementById('time').innerHTML = h + ':' + m;
 
 // setTimeout(10000);
